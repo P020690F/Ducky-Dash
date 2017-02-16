39d94c88e7ba92b35a30697a6c0636f243ee5ea7
Thomas Griffiths <Arceusavenger@gmail.com>
2017-02-16 17:41:28 +0000

    Includes the start of the upgrade code, as well as a placeholder image for the game over screen.

---
+++ Upgrades.lua
@@ -0,0 +1,18 @@
+function HalfSpeed()
+  
+  
+end
+function DoublePoints()
+  while (upgradeState = "double") then
+    points = points * 2
+        
+  
+end
+function LifeLine()
+  
+end
+
+function Invincibility()
+  
+  
+  end

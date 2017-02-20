d0b442f0d9049076f0fba790da6f78f82a664a60
p020690f <p020690f@student.staffs.ac.uk>
2017-02-20 12:50:27 +0000

    Remove debug text in menu

--- Main.lua
+++ Main.lua
@@ -5,17 +5,14 @@ require "Sound"
 require "Store"
 
 function love.load()
-  if arg[#arg] == "-debug" then require("mobdebug").start() end
+  --if arg[#arg] == "-debug" then require("mobdebug").start() end
   
-  gamestate = "endless"
+  gamestate = "menu"
   Game.load()
   Menu.load()
   Sound.load()
   Store.load()
   Sound.play()
-  
-  clickX = 0
-  clickY=0
   
   screenWidth = 750/2
   screenHeight = 1337/2
@@ -52,19 +49,10 @@ function love.draw()
   if gamestate == "store" then
     Store.drawDuckSelect()
   end
-
-  love.graphics.setColor(0,0,0)
-  love.graphics.print(clickX,100,200)
-  love.graphics.print(clickY,100,250)
-  love.graphics.setColor(255,255,255)
 end
 
 function love.update()
   love.graphics.scale(scalex, scaley)
-  
-  if gamestate == "menu" then
-    Menu.update()
-  end
   
   if gamestate == "story" then
     Menu.updateStory()
@@ -76,24 +64,20 @@ function love.update()
 end
 
 function love.mousepressed(x,y,button,istouch)
-  if button == 1 then
-    clickX = x
-    clickY = y
+  if (gamestate == "menu") then
+    --story mode main screen duck
+    if (x < 95 and y > 355 and y <455) then
+      _G.completedStory = true
+    end 
+
+  --duck shop 
+    if (x > 140 and x < 235 and y > 345 and y <450) then
+    
+    end 
+
+  -- endless mode
+    if (x > 265 and x < 360 and y > 345 and y <450 and _G.completedStory) then
+    
+    end 
   end
-
-  --story mode main screen duck
-  if (x < 95 and y > 355 and y <455) then
-    _G.completedStory = true
-  end 
-
---duck shop 
-  if (x > 140 and x < 235 and y > 345 and y <450) then
-  
-  end 
-
--- endless mode
-  if (x > 265 and x < 360 and y > 345 and y <450 and _G.completedStory) then
-  
-  end 
-
 end

4ef9e9cb795faebbddea16b1a6697882f57ca472
P020690F <p020690f@student.staffs.ac.uk>
2017-03-04 12:06:17 +0000

    Initial merge with branch Rhys-O #More changes needed #Merge with game.lua & menu.lua required

---
+++ pause.lua
@@ -0,0 +1,83 @@
+module("Pause", package.seeall)
+require "main"
+
+function load()
+  resumeImg = love.graphics.newImage("assets/Resume_Button.png")
+  retryImg = love.graphics.newImage("assets/RetryButton.png")
+  settingsImg = love.graphics.newImage("assets/Settings_Button.png")
+  mainImg = love.graphics.newImage("assets/MainMenuButton.png")
+  
+  buttonQuad = love.graphics.newQuad(1,1,500/2,500/2,500/2,500/2)
+end
+
+function draw()
+  x = love.mouse.getX()
+  love.graphics.print(x, 10 , 10)
+  love.graphics.print(love.mouse.getY(), 50 , 10)
+  
+  love.graphics.draw(resumeImg, buttonQuad, 125/2 ,0)
+  love.graphics.draw(retryImg, buttonQuad, 125/2 ,250/2)
+  love.graphics.draw(settingsImg, buttonQuad, 125/2 ,500/2)
+  love.graphics.draw(mainImg, buttonQuad, 125/2 ,750/2)
+  
+end
+
+function mousepressed(x,y,button, istouch)
+
+  if x > 85 and x < 288 then
+  
+    if y > 70 and y < 176 then
+      -- resume
+      _G.paused = false
+    end
+  
+    if y > 196 and y < 302 then
+      -- retry
+   
+    end
+  
+    if y > 320 and y < 426 then
+      -- settings
+   
+    end
+  
+    if y > 446 and y < 552 then
+      -- menu
+      
+      --doesnt reset game 
+      _G.paused = false
+      main.gamestate = "menu"
+    end
+  
+  end
+end
+
+function touchpressed(id,x,y,sw,sh,pressure)
+
+  if x > 85 and x < 288 then
+  
+    if y > 70 and y < 176 then
+      -- resume
+      _G.paused = false
+    end
+  
+    if y > 196 and y < 302 then
+      -- retry
+   
+    end
+  
+    if y > 320 and y < 426 then
+      -- settings
+   
+    end
+  
+    if y > 446 and y < 552 then
+      -- menu
+      
+      --doesnt reset game 
+      _G.paused = false
+      main.gamestate = "menu"
+    end
+  
+  end
+end
--- store.lua
+++ store.lua
@@ -3,13 +3,59 @@ require "main"
 
 function load()
   bathtub = love.graphics.newImage("assets/Bathtub.png")
+  PDFront = love.graphics.newImage("assets/Duck Skins/Punk_Duck_Front.png")
   backgroundQuad = love.graphics.newQuad(1,1,750/2,1337/2,750/2,1337/2)
+
+  
+  smallDuckQuad = love.graphics.newQuad(1,1,150/2,150/2,150/2,150/2)
+  centreDuckQuad = love.graphics.newQuad(1,1,450/2,450/2,500/2,500/2)
+  
+  storeState = "Duck"
+
+
 end
+
+function drawStoreHub()
+  
+  if (storeState == "Duck") then
+    drawDuckSelect()
+  end
+  
+  if (storeState == "PowerUp") then
+    drawPowerUpStore()
+  end
+end
+
+function clickStoreHub()
+
+
+end
+
+function pressStoreHub(id,x,y,sw,sh,pressure)
+
+
+end
+
+
+
 
 function drawDuckSelect()
   love.graphics.draw(bathtub, backgroundQuad, 0, 0)
+  love.graphics.draw(PDFront, centreDuckQuad, 125/2 ,450/2)
+  
+  --smaller behind ducks
+  love.graphics.setColor(113,113,133)
+  love.graphics.draw(PDFront, smallDuckQuad, 100/2 ,250/2)
+  love.graphics.draw(PDFront, smallDuckQuad, 500/2 ,250/2)
+  love.graphics.setColor(255,255,255)
+  
+  
 end  
 
 function drawPowerUpStore()
-  love.graphics.draw(bathtub, backgroundQuad, 0, 0)
+love.graphics.draw(bathtub, backgroundQuad, 0, 0)
+end
+
+function updateStore()
+
 end
